0fc1f8cd968e15e2f94af0894514319224715814
Carsten Behring <carsten.behring@gmail.com>
2015-07-13 10:55:10 +0200

    removed old setup

--- elpa/smex-20131210.700/smex-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; smex-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (smex-initialize smex) "smex" "smex.el" (21190
-;;;;;;  53704 569463 222000))
-;;; Generated autoloads from smex.el
-
-(autoload 'smex "smex" "\
-
-
-\(fn)" t nil)
-
-(autoload 'smex-initialize "smex" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("smex-pkg.el") (21190 53704 599235 82000))
-
-;;;***
-
-(provide 'smex-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; smex-autoloads.el ends here
