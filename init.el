dcc2529f7b435a48888b6187cf23daa147670efa
Carsten Behring <carsten.behring@gmail.com>
2015-12-20 16:41:51 +0100

    added date

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 (package-initialize)
 
 (require 'ob-tangle)
-;;(setq debug-on-error t)
+					;(setq debug-on-error t)
 (org-babel-load-file
  (expand-file-name "init/init.org"
                    user-emacs-directory))
